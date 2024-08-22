b905abaaa24f92ecde45746414a4afbcf1d98557
ouegy <ianbutler82@yahoo.co.uk>
2024-08-22 12:42:08 +1000

    UI and displayBook functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -13,65 +15,60 @@ function Book(title, author, pages, read) {
     };
 }
 
-let bookObject = Object.getPrototypeOf(Book);
-console.log(bookObject);
+const lotr = new Book('The Lord of the Rings', 'JRR Tolkein', '565 pages', true);
+const theHobbit = new Book('The Hobbit', 'JRR Tolkien', '295 pages', false);
+
+function addBookToLibrary() {
+    myLibrary.push(theHobbit);
+    myLibrary.push(lotr);
+}
+
+const getBookInfo = (text) => {
+
+}
+
+function displayBooks(array) {
+    array.forEach(book => {
+        const title = book.title;
+        let author = book.author;
+        let pages = book.pages;
+        let read = book.read;
+        let index = array.indexOf(book);
+
+        const mainDiv = document.getElementById("main");
+        const div = document.createElement('div');
+        const buttons = document.createElement('div');
+        const button1 = document.createElement('button');
+        const button2 = document.createElement('button');
+
+        const createBookCard = (book) => {
+            div.setAttribute('class', 'card');
+            div.setAttribute('data-attribute', index);
+            buttons.setAttribute('class', 'buttons');
+            button1.appendChild(document.createTextNode('Remove Book'));
+            button2.appendChild(document.createTextNode('Update Read Status'));
+            buttons.appendChild(button1);
+            buttons.appendChild(button2);
+            mainDiv.appendChild(div);
+            div.appendChild(buttons);
+        }
+
+        createBookCard();
+
+        const getBookInfo = (text) => {
+            const h2 = document.createElement('h2');
+            div.appendChild(h2);
+            h2.appendChild(document.createTextNode(text));
+        }
+
+        const arr = [`Title: ${title}`, `Author: ${author}`, `Pages: ${pages}`, `Read: ${read}`];
+        arr.forEach(element => getBookInfo(element));
+
+    });
+}
 
 
+addBookToLibrary();
+displayBooks(myLibrary);
 
-const theHobbit = new Book('The Hobbit', 'JRR Tolkien', '295 pages', 'read');
-
-console.log(theHobbit.info());
-console.log(Object.getPrototypeOf(theHobbit));
-document.write(theHobbit.title, theHobbit.author, theHobbit.pages, theHobbit.read);
-
-let animal = {
-    eats: true
-  };
-  let rabbit = {
-    jumps: true
-  };
-Object.setPrototypeOf(rabbit, animal); // sets rabbit.[[Prototype]] = animal
-  
-console.log( rabbit.eats ); // true (**)
-console.log( rabbit.jumps ); // true
-
-let head = {
-    glasses: 1
-  };
-  
-  let table = {
-    pen: 3
-  };
-  
-  let bed = {
-    sheet: 1,
-    pillow: 2
-  };
-  
-  let pockets = {
-    money: 2000
-  };
-  
-  Object.setPrototypeOf(table, head);
-  Object.setPrototypeOf(bed, table);
-  Object.setPrototypeOf(pockets, bed);
-
-  console.log(pockets.pen);
-
-// function Player(name, marker) {
-//     this.name = name;
-//     this.marker = marker;
-//     this.sayName = function() {
-//       console.log(this.name)
-//     };
-//   }
-  
-//   const player1 = new Player('steve', 'X');
-//   const player2 = new Player('also steve', 'O');
-//   player1.sayName(); // logs 'steve'
-//   player2.sayName(); // logs 'also steve'
-
-//   console.log(Object.getPrototypeOf(player1));
-
-
-  
+console.table(myLibrary);
